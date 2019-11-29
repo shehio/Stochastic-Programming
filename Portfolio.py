d7f6b52ed2b2f4c627edbab59a668df676a5c1d3
shehio <shehabyasser@gmail.com>
2019-11-29 14:08:12 -0800

    Added log normal generation for returns.

--- Portfolio.py
+++ Portfolio.py
@@ -1,21 +1,27 @@
 import numpy as np
+import numpy.random as nr
+
+EPS = 0.0001
 
 
 class Portfolio:
-    means = 0
-    variance = 0
-    fees = 0
-    standard_deviations = []
-    correlations = [[]]
 
     def __init__(self, weights, means, standard_deviations, correlations, fees):
-        assert np.sum(weights) == 1
+        assert np.sum(weights) > 1 - EPS
+        assert np.sum(weights) < 1 + EPS
         self.weights = weights
         self.means = means
         self.standard_deviations = standard_deviations
         self.correlations = correlations
         self.fees = fees
+        self.std_diag = np.diag(self.standard_deviations)
+        self.covariance = self.std_diag * self.correlations * self.std_diag
+        self.mu_log = np.log(self.means)
+
         return
 
     def sample_return(self):
-        return round(np.dot(self.weights, self.means) * (1 - self.fees), 2)
+        multivariate_normal = nr.multivariate_normal(self.mu_log, self.covariance)
+        multivariate_log_normal = np.exp(multivariate_normal)
+        returns = round(np.dot(self.weights, multivariate_log_normal) * (1 - self.fees), 2)
+        return returns
--- PortfolioFactory.py
+++ PortfolioFactory.py
@@ -1,11 +1,47 @@
 import Portfolio
-
+import numpy as np
 
 class PortfolioFactory:
 
     @staticmethod
     def get_available_portfolios():
-        portfolios = [Portfolio.Portfolio([0.5, 0.5], [0.10, 0.20], [0.20, 0.40], [[1, 0.3], [0.3, 1]], 0.2),
-                      Portfolio.Portfolio([0.7, 0.3], [0.10, 0.20], [0.20, 0.40], [[1, 0.3], [0.3, 1]], 0.2),
-                      Portfolio.Portfolio([0, 1], [0.10, 0.20], [0.20, 0.40], [[1, 0.3], [0.3, 1]], 0.2)]
+        means = [0.06, 0.059, 0.07, 0.056, 0.019, 0.052, 0.015]
+        standard_deviations = [0.091, 0.202, 0.268, 0.207, 0.038, 0.07, 0.058]
+        cor = np.array([[1.000, 0.740, 0.670, 0.740, 0.130, 0.470, 0.020],
+                        [0.740, 1.000, 0.700, 0.780, 0.090, 0.460, 0.000],
+                        [0.670, 0.700, 1.000, 0.660, 0.070, 0.450, -0.03],
+                        [0.740, 0.780, 0.660, 1.000, 0.100, 0.370, -0.03],
+                        [0.130, 0.090, 0.070, 0.100, 1.000, 0.100, 0.100],
+                        [0.470, 0.460, 0.450, 0.370, 0.100, 1.000, 0.550],
+                        [0.020, 0.000, -0.03, -0.03, 0.100, 0.550, 1.000]])
+
+        weights1 = [0.3, 0.3, 0.1, 0.1, 0.1, 0.1, 0]
+        fees1 = 0.2
+
+        weights2 = [0.3, 0.3, 0.1, 0.1, 0.1, 0.1, 0]
+        fees2 = 0.2
+
+        weights3 = [0.3, 0.3, 0.1, 0.1, 0.1, 0.1, 0]
+        fees3 = 0.2
+
+        portfolios = [
+            Portfolio.Portfolio(
+                weights = weights1,
+                means = means,
+                standard_deviations = standard_deviations,
+                correlations = cor,
+                fees = fees1),
+            Portfolio.Portfolio(
+                weights = weights2,
+                means = means,
+                standard_deviations = standard_deviations,
+                correlations = cor,
+                fees = fees2),
+            Portfolio.Portfolio(
+                weights = weights3,
+                means = means,
+                standard_deviations = standard_deviations,
+                correlations = cor,
+                fees = fees3),
+        ]
         return portfolios
