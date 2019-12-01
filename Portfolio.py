81550aa53253260b98f5a8a472f7b9f105c6563f
shehio <shehabyasser@gmail.com>
2019-12-01 09:42:36 -0800

    Modified the portfolio even more.

--- Portfolio.py
+++ Portfolio.py
@@ -14,15 +14,13 @@ class Portfolio:
         self.standard_deviations = standard_deviations
         self.correlations = correlations
         self.fees = fees
-        self.std_diag = np.diag(self.standard_deviations)
-        self.covariance = self.std_diag * self.correlations * self.std_diag
-        self.mu_log = np.log(self.means)
-
+        std_diag = np.diag(self.standard_deviations)
+        self.covariance = std_diag * self.correlations * std_diag
         return
 
     def sample_return(self):
         multivariate_normal = nr.multivariate_normal(self.means, self.covariance)
-        multivariate_log_normal = np.exp(multivariate_normal) - 1  # He is saying there should be a -1 here.
+        multivariate_log_normal = np.exp(multivariate_normal) - 1
 
         returns = round(np.dot(self.weights, multivariate_log_normal) * (1 - self.fees), 3)
         return returns
